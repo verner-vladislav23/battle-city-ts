ac06a8a2a539be5159e563adc0a53316cc1fab9f
Verner <verner.vladislav23@gmail.com>
2022-05-01 16:12:24 +0300

    init webpack

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
 const t: number = 6;
 
-function testTestTestTEstetsetestestet(
-  arg1: string,
-  arg2: string,
-  arg3: string,
-  arg4: string,
-) {
-  const fn = () => {
-    const fn1 = () => {
-      const fn2 = () => {
-        return null;
-      };
-    };
-  };
-}
-
 console.log(t);
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  target: ['web'],
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle-[chunkhash].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  devServer: {
+    static:  {
+      directory: 'src/index.html',
+      watch: true,
+    },
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      // {
+      //   test: /\.(svg|woff|woff2|ttf|eot|otf)([\?]?.*)$/,
+      //   use: [
+      //     {
+      //       loader: 'file-loader?name=assets/fonts/[name].[ext]',
+      //     },
+      //   ],
+      // },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style-[hash].css',
+    }),
+  ],
+};
