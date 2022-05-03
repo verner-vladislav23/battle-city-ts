ef72957d36e9bb4ca56bd30c6359d7e45b749e18
Verner <verner.vladislav23@gmail.com>
2022-05-03 16:33:54 +0300

    canvas entity

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
+import Canvas from './lib/canvas/Canvas';
+
 window.addEventListener("load", initCanvas);
 
 function initCanvas() {
   const body = document.querySelector("body");
-  const canvas = document.createElement("canvas");
 
-  canvas.style.display = "block";
-  canvas.style.marginLeft = "auto";
-  canvas.style.marginRight = "auto";
-  canvas.style.marginTop = "50px";
-  canvas.style.border = "1px solid black";
-  canvas.height = 500;
-  canvas.width = 800;
+  const canvas = new Canvas({
+    width: 800,
+    height: 500,
+  });
 
-  body?.appendChild(canvas);
+  body?.appendChild(canvas.node);
 }
