eac06e00b04d72e3c1e791d2db54be740191e0f0
Verner <verner.vladislav23@gmail.com>
2022-05-11 10:52:56 +0300

    calcualte start of bullets

--- src/models/Bullet/Bullet.ts
+++ src/models/Bullet/Bullet.ts
@@ -2,7 +2,7 @@ import Motion from '../Motion/Motion';
 import { Position } from '../../types/position';
 import { Render } from '../../dom/canvas/renders';
 import { IBullet } from './interface';
-import { TANK_DIRECTION } from '../Tank/constants';
+import { TANK_DIRECTION, TANK_HEIGHT, TANK_WIDTH } from '../Tank/constants';
 import { TankDirection } from '../Tank/types';
 import { BULLET_STEP, BULLET_WIDTH, BULLET_HEIGHT } from './constants';
 
@@ -10,14 +10,49 @@ export default class Bullet extends Motion implements IBullet {
   private readonly updatePositionIntervalId: number;
   private tick: number;
 
-  constructor(public position: Position, direction: TankDirection) {
-    super(position, BULLET_HEIGHT, BULLET_WIDTH, BULLET_STEP);
+  constructor(public tankPosition: Position, tankDirection: TankDirection) {
+    super(tankPosition, BULLET_HEIGHT, BULLET_WIDTH, BULLET_STEP);
+    this.position = this.calculateBulletStartPosition(tankPosition, tankDirection);
     this.tick = 0;
 
     this.updatePositionIntervalId = window.setInterval(() => {
-      this.updatePositionByTankDirection(direction);
+      this.updatePositionByTankDirection(tankDirection);
       this.render();
     }, 5);
+  }
+
+  private calculateBulletStartPosition(
+    tankPosition: Position,
+    tankDirection: TankDirection,
+  ): Position {
+    switch (tankDirection) {
+      case TANK_DIRECTION.UP: {
+        return {
+          x: (tankPosition.x + TANK_WIDTH / 2) - (BULLET_HEIGHT / 2),
+          y: tankPosition.y - BULLET_HEIGHT,
+        }
+      }
+      case TANK_DIRECTION.RIGHT: {
+        return {
+          x: (tankPosition.x + TANK_WIDTH),
+          y: (tankPosition.y + TANK_HEIGHT / 2) - (BULLET_HEIGHT / 2),
+        };
+      }
+      case TANK_DIRECTION.LEFT: {
+        return {
+          x: tankPosition.x - BULLET_WIDTH,
+          y: (tankPosition.y + TANK_HEIGHT / 2) - (BULLET_HEIGHT / 2),
+        }
+      }
+      case TANK_DIRECTION.DOWN: {
+        return {
+          x: (tankPosition.x + TANK_WIDTH / 2) - (BULLET_HEIGHT / 2),
+          y: tankPosition.y + TANK_HEIGHT,
+        };
+      }
+    }
+
+    return tankPosition;
   }
 
   private updatePositionByTankDirection(tankDirection: TankDirection): void {
